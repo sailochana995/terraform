f7c06f1b60598c2d14d50903738da4259bf6c967
sailochana995 <sailochana99@gmail.com>
2025-04-23 10:35:06 +0530

    adding conditions file

---
+++ conditions/variable.tf
@@ -0,0 +1,45 @@
+variable  "ami_id" {
+
+  type = string
+
+  default = "ami-09c813fb71547fc4f"
+}
+
+variable "environment" {
+    default = "prod"
+}
+
+variable "instance_type"  {
+
+default = "t2.micro"
+}
+
+variable "ec2_tags" {
+
+  type = map 
+  default = {
+    project = "expense"
+    component = "backend"
+    environment = "dev"
+    name = "expense-backend-dev"
+  }
+}
+variable  "from_port" {
+  type = number
+  default = 22
+}
+
+variable  "to_port" {
+type = number
+default = 22
+}
+variable  "cidr_blocks" {
+  type = list
+  default = ["0.0.0.0/0"]
+}
+variable "sg_tags" {
+  type = map 
+  default = {
+    name = "expense-backend-dev"
+  }
+}
