cc1898d0804b3f605786464f99bf943695307fc4
sailochana995 <sailochana99@gmail.com>
2025-04-24 16:21:32 +0530

    count added

---
+++ count/variable.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+    default = ["mysql", "backend", "frontend"]
+}
+
+variable "zone_id" {
+    default = "Z06951323BQNCIM51LI4E"
+}
+
+variable "domain_name" {
+    default = "awsdevops82s.online"
+}
