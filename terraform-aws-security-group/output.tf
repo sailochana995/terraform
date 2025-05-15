7a54555e04df547ab3238ebdacf2b63d2b3941d2
sailochana995 <sailochana99@gmail.com>
2025-05-15 20:19:01 +0530

    Delete terraform-aws-security-group directory

--- terraform-aws-security-group/output.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
